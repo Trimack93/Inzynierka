01e8b0877481b4b7aeb9d379bd0d952698879448
Trimack93 <trimack93@gmail.com>
2015-11-21 02:26:37 +0100

    GUI for Graph Generator
    - New project with graph generator
    - Custom GroupBox style with centered header
    - Test of databinding to individual elements of ObservableCollection
    - Command test
    - New tooltips
    - Extended tooltip visibility time

---
+++ GraphGenerator/ViewModels/MainViewModel.cs
@@ -0,0 +1,67 @@
+﻿using GraphGenerator.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace GraphGenerator.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<SolidColorBrush> _Colors = new ObservableCollection<SolidColorBrush>();
+        public ObservableCollection<SolidColorBrush> Colors
+        {
+            get { return _Colors; }
+        }
+
+        public MainViewModel()
+        {
+            _Colors.Add( Brushes.Green );
+            _Colors.Add( Brushes.Red );
+            _Colors.Add( Brushes.Green );
+            _Colors.Add( Brushes.Green );
+            _Colors.Add( Brushes.Red);
+            _Colors.Add( Brushes.Green );
+            _Colors.Add( Brushes.Red);
+        }
+
+        // GUI update test
+        void ChangeCompatibilityExecute()
+        {
+            if (_Colors[2] == Brushes.Green)
+                _Colors[2] = Brushes.Red;
+            else
+                _Colors[2] = Brushes.Green;
+        }
+
+        bool CanChangeCompatibility()
+        {
+            return true;
+        }
+
+        public ICommand ChangeCompatibility
+        {
+            get { return new RelayCommand(ChangeCompatibilityExecute, CanChangeCompatibility); }
+        }
+
+        #region INotifyPropertyChanged
+        //---------------------------------------
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;              // Take a copy to prevent thread issues
+
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+}
